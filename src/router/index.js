fb11d8d77995e7d89237c681d9e1895f861ccadb
uljf45 <uljf45@gmail.com>
2018-03-03 21:06:09 +0800

    lesson5

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ export default new Router({
     {
       path: '/',
       // name: 'HelloWorld',
-      component: Recommend
+      redirect: '/recommend'
     },
     {
       path: '/recommend',
